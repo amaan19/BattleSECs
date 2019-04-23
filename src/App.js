975dfe0ab941308624a6c7811ba0a724797a5b59
GCdM <gabriel.miranda@flatironschool.com>
2019-04-23 17:47:16 +0100

    created GameBoard with OpponentBoard and PlayaBoard, and added selectedTile functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
 
-import BoardTile from './components/BoardTile'
-import { boardData } from './data'
+import GameBoard from './containers/GameBoard'
 import './App.css';
 
 class App extends Component {
-  state = {
-    board: [],
-  }
-
-  componentDidMount() {
-    this.setState({ board: boardData })
-  }
-
-  createRow = (rowArray) => {
-    let key = 1
-    return <div key={this.state.board.indexOf(rowArray)} className="test-row">
-      {rowArray.map( cell => <BoardTile key={key++} ></BoardTile> )}
-    </div>
-  }
+  
 
   render() {
     return (
       <div className="App">
-        {this.state.board.map( this.createRow )}
+        <GameBoard />
       </div>
     );
   }
--- src/data.js
+++ src/data.js
@@ -1,5 +1,11 @@
-export const boardData = [
-  [{ hit: false, ship: false }, { hit: false, ship: false }, { hit: false, ship: false }],
-  [{ hit: false, ship: false }, { hit: false, ship: false }, { hit: false, ship: false }],
-  [{ hit: false, ship: false }, { hit: false, ship: false }, { hit: false, ship: false }],
+export const opponentBoard = [
+  [{ hit: false, sucessful: false }, { hit: false, sucessful: false }, { hit: false, sucessful: false }],
+  [{ hit: true, sucessful: false }, { hit: false, sucessful: true }, { hit: false, sucessful: false }],
+  [{ hit: true, sucessful: true }, { hit: false, sucessful: false }, { hit: false, sucessful: false }],
+]
+
+export const playaBoard = [
+  [{ hit: false, ship: true }, { hit: false, ship: false }, { hit: false, ship: false }],
+  [{ hit: false, ship: true }, { hit: false, ship: false }, { hit: true, ship: true }],
+  [{ hit: false, ship: true }, { hit: false, ship: false }, { hit: true, ship: false }],
 ]
